ae046747d1ce11d51a260d5d7a76bdab6fea0f09
Michael McClimon <michael@mcclimon.org>
2020-02-14 09:17:55 -0500

    docs: Ix::DBIC::AccountResult and ::StatesResult

--- lib/Ix/DBIC/AccountResult.pm
+++ lib/Ix/DBIC/AccountResult.pm
@@ -1,10 +1,48 @@
 use 5.20.0;
 use warnings;
 package Ix::DBIC::AccountResult;
+# ABSTRACT: easily get resultsets bound to an accountId
 
 use parent 'DBIx::Class';
 
 use experimental qw(signatures postderef);
+
+=head1 SYNOPSIS
+
+    package MyApp::Schema::Result::Foo;
+    use base qw/DBIx::Class::Core/;
+
+    __PACKAGE__->load_components(qw/+Ix::DBIC::AccountResult/);
+
+    __PACKAGE__->add_columns(
+      id        => { data_type => 'uuid' },
+      accountId => { data_type => 'uuid' },
+      ...
+    );
+
+    # Elsewhere...
+    package MyApp::Some::Other::Package;
+
+    my $rs  = $ctx->account_rs('Foo')->search(...);
+    my $all = $ctx->account_rs_including_inactive('Foo')->search(...);
+
+Mostly these methods are called via an L<Ix::Context::WithAccount> object.
+
+=head1 DESCRIPTION
+
+We wrote this component after forgetting one too many times to include
+C<accountId> and C<isActive> in a DBIC search, which led to one very visible bug.
+This component provides two methods, C<account_rs> and
+C<account_rs_including_inactive>, which automatically add C<< $self->accountId >>.
+If the resultset is an L<Ix::DBIC::Result>, the first also adds
+C<< isActive => 1 >>.
+
+=method account_rs($rs_name)
+
+Equivalent to C<< $schema->resultset($rs_name) >>, but limits to the active
+C<accountId> and, if relevant, C<isActive>.
+
+=cut
 
 sub account_rs ($self, $rs_name) {
   my $rs = $self->result_source->schema->resultset($rs_name)->search({
@@ -17,6 +55,12 @@ sub account_rs ($self, $rs_name) {
 
   return $rs;
 }
+
+=method account_rs_including_inactive($rs_name)
+
+Exactly like the above, but without the C<isActive> constraint.
+
+=cut
 
 sub account_rs_including_inactive ($self, $rs_name) {
   return $self->result_source->schema->resultset($rs_name)->search({
--- lib/Ix/DBIC/StatesResult.pm
+++ lib/Ix/DBIC/StatesResult.pm
@@ -1,10 +1,30 @@
 use 5.20.0;
 use warnings;
 package Ix::DBIC::StatesResult;
+# ABSTRACT: the DBIC result for your states
 
 use parent 'DBIx::Class';
 
 use experimental qw(signatures postderef);
+
+=head1 SYNOPSIS
+
+This is the DBIC result class for all of your application's JMAP state strings.
+Probably you can just copy and paste this exactly.
+
+    package MyApp::Schema::Result::State;
+    use base qw/DBIx::Class::Core/;
+
+    __PACKAGE__->load_components(qw/+Ix::DBIC::StatesResult/);
+    __PACKAGE__->table('states');
+    __PACKAGE__->ix_setup_states_result;
+
+=method ix_setup_states_result
+
+Does what it says on the tin. Adds four columns: accountId, type,
+lowestModSeq, and highestModSeq.
+
+=cut
 
 sub ix_setup_states_result ($class) {
   $class->add_columns(
