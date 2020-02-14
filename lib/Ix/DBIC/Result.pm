d92f4af3dc00308ee3e662f1bb35097d55a07d20
Michael McClimon <michael@mcclimon.org>
2020-02-14 15:24:03 -0500

    docs: Ix::DBIC::Result
    Oof, lots here!

--- lib/Ix/DBIC/Result.pm
+++ lib/Ix/DBIC/Result.pm
@@ -1,6 +1,7 @@
 use 5.20.0;
 use warnings;
 package Ix::DBIC::Result;
+# ABSTRACT: a DBIC result class with JMAP smarts
 
 use parent 'DBIx::Class';
 
@@ -11,27 +12,124 @@ use Ix::Validators;
 use Ix::Util qw(ix_new_id);
 use JSON::MaybeXS;
 
+=head1 SYNOPSIS
+
+    package MyApp::Schema::Result::Cookie;
+    use base qw/DBIx::Class::Core/;
+
+    __PACKAGE__->load_components(qw/+Ix::DBIC::Result/);
+
+    __PACKAGE__->table('cookies');
+
+    __PACKAGE__->ix_add_columns;
+
+    __PACKAGE__->ix_add_properties(
+      batch      => { data_type => 'integer', client_may_init => 0, client_may_update => 0 },
+      ...
+    );
+
+    __PACKAGE__->set_primary_key('id');
+
+    sub ix_type_key { 'Cookie' }
+
+    sub ix_account_type { 'generic' }
+
+    ...
+
+=head1 OVERVIEW
+
+This class is where Ix gets most of its smarts. In your application, you define
+C<DBIx::Class> result classes (here, rclasses) and load this class as a
+component. Doing so gives you many extra behaviors, and allows you to hook into
+various points of the operations done by L<Ix::DBIC::ResultSet> in its
+implementations of the core JMAP methods.
+
+=cut
+
 __PACKAGE__->load_components(qw/+Ix::DBIC::AccountResult/);
 
+=method ix_account_type
+
+You B<must> define this method in your rclass. This is the account type that's
+passed to C<with_account> in L<Ix::Context>.
+
+(In theory, an Ix application can have multiple account types, but in practice,
+we have only ever used a single one.)
+
+=cut
+
 sub ix_account_type { Carp::confess("ix_account_type not implemented") }
+
+=method ix_type_key
+
+You B<must> define this method in your rclass. This is the JMAP noun associated
+with this rclass (e.g., the type key for 'Foo/get' is 'Foo').
+
+=cut
 
 # Checked for in ix_finalize
 # sub ix_type_key { }
 
+=method ix_query_enabled
+
+If this returns a true value, the JMAP processor will generate handlers for
+'Foo/query' and 'Foo/queryChanges'. The default is false, so these methods
+will not be generated unless you ask.
+
+=cut
+
 sub ix_query_enabled {}
+
+
+=method ix_extra_get_args
+
+If provided by an rclass, this should return a list of keys that clients are
+allowed to pass as arguments to 'Foo/get' (in addition to the JMAP-defined
+I<ids> and I<properties>).
+
+=cut
+
 sub ix_extra_get_args { }
 
-# Must be specified if the rclass represents a new 'account'
+=method ix_account_base
+
+This must return true if this rclass represents a new 'account'.
+
+=cut
+
 sub ix_is_account_base { 0 }
+
+=method ix_virtual_property_names
+
+This returns a list of properties defined as C<is_virtual> in the call to
+C<ix_add_properties>.
+
+=cut
 
 sub ix_virtual_property_names ($self, @) {
   my $prop_info = $self->ix_property_info;
   return grep {; $prop_info->{$_}{is_virtual} } keys %$prop_info;
 }
 
+=method ix_property_names
+
+This returns a list of all property names defined in the call to
+C<ix_add_properties>.
+
+=cut
+
 sub ix_property_names ($self, @) {
   return keys $self->ix_property_info->%*;
 }
+
+=method ix_client_update_ok_properties($ctx)
+
+This returns a list of all the property names allowed to be updated by C<$ctx>.
+For normal contexts, this includes all properties defined as
+C<client_may_update>, minus any virtual or immutable properties. System contexts
+may update non-updateable properties, but not virtual or immutable ones.
+
+=cut
 
 sub ix_client_update_ok_properties ($self, $ctx) {
   my $prop_info = $self->ix_property_info;
@@ -50,6 +148,15 @@ sub ix_client_update_ok_properties ($self, $ctx) {
     keys %$prop_info;
 }
 
+=method ix_client_init_ok_properties($ctx)
+
+This returns a list of all the property names allowed to be initialized by C<$ctx>.
+For normal contexts, this includes all properties defined as
+C<client_may_init>, minus any virtual or immutable properties. System contexts
+may always instantiate properties.
+
+=cut
+
 sub ix_client_init_ok_properties ($self, $ctx) {
   my $prop_info = $self->ix_property_info;
 
@@ -62,6 +169,13 @@ sub ix_client_init_ok_properties ($self, $ctx) {
     keys %$prop_info;
 }
 
+=method ix_default_properties
+
+Rclasses can override this to define default properties. It should return a
+hashref of C<< property_name => 'value' >>.
+
+=cut
+
 sub ix_default_properties { return {} }
 
 sub new ($class, $attrs) {
@@ -72,6 +186,13 @@ sub new ($class, $attrs) {
 
   return $class->next::method($attrs);
 }
+
+=method ix_add_columns
+
+This method adds all of the default columns for JMAP operations: id, accountId,
+created, modSeqCreated, modSeqChanged, dateDestroyed, and isACtive.
+
+=cut
 
 sub ix_add_columns ($class) {
   $class->ix_add_properties(
@@ -92,14 +213,24 @@ sub ix_add_columns ($class) {
   );
 }
 
-# Since ix_destroy doesn't actually delete rows, we need a way for unique
-# constraints to work while letting rows stick around. What we're doing is
-# injecting a column (isActive) into the beginning of the unique constraints
-# that has only two possible values: true on create, and NULL when the row
-# is destroyed. While the value is true, it allows the unique constraint to work.
-# When the value becomes NULL, it will no longer ever match any other rows, and
-# so will not get in the way of active data (in Postgres, NULL is never equal
-# to NULL).
+=method ix_add_unique_constraint($constraint)
+
+=method ix_add_unique_constraints(@constraints)
+
+These methods are wrappers around DBIC's C<add_unique_constraints>, but add
+C<isActive> to the constraint. This is necessary because C<ix_destroy> does not
+actually destroy rows, but simply I<marks> them destroyed. That means if you
+were to add a constraint, then destroy the row, you could never add a matching
+row again. Using these methods makes sure that doesn't happen.
+
+=cut
+
+# What we're doing here is injecting a column (isActive) into the beginning of
+# the unique constraints that has only two possible values: true on create, and
+# NULL when the row is destroyed. While the value is true, it allows the unique
+# constraint to work.  When the value becomes NULL, it will no longer ever match
+# any other rows, and so will not get in the way of active data (in Postgres,
+# NULL is never equal to NULL).
 sub ix_add_unique_constraints ($class, @constraints) {
   for my $c (@constraints) {
     if (ref $c) {
@@ -113,6 +244,63 @@ sub ix_add_unique_constraints ($class, @constraints) {
 sub ix_add_unique_constraint ($class, @constraint) {
   $class->ix_add_unique_constraints(@constraint);
 }
+
+=method ix_add_properties(@pairs)
+
+This is the method used to add properties (columns) to an rclass. It is mostly a
+wrapper around DBIC's C<add_columns>, but does additional housekeeping so that
+we can generate JMAP handlers with these properties later.
+
+An example will be useful:
+
+    package MyApp::Schema::Result::Cookie;
+    use Ix::Validators qw(integer);
+
+    __PACKAGE__->ix_add_properties(
+      batch       => { data_type => 'integer', client_may_init => 0, client_may_update => 0 },
+      baked_at    => { data_type => 'timestamptz', is_optional => 1 },
+      stale_date  => { data_type => 'timestamptz', is_virtual => 1 },
+      batch_size  => { data_type => 'integer', is_optional => 1, validator => integer(0, 100) },
+
+    );
+
+This code adds four properties to our rclass:
+
+=for :list
+1. batch - an integer, not updateable or initializable by the client
+2. baked_at - an optional timestamp
+3. stale_date - a virtual property; no database column is added
+4. batch_size - an optional integer, which must be between 0 and 100.
+
+C<data_type> defines the type of the column, and also provides automatic
+validation with L<Ix::Validator>. They're shown here with their corresponding
+Postgres types and which validator they use:
+
+=for :list
+
+* string - text, validated with C<simplestr>
+* istring - citext, validated with C<simplestr>,
+* timestamptz - timestamptz, not validated by Ix
+* string[] - text[], not currently validated by Ix
+* boolean - boolean, validated with C<boolean>
+* integer - integer, is_numeric = 1, validated with C<integer>
+* idstr - uuid, validated with C<idstr>.
+
+Other keys you might use in a column definition include:
+
+=for :list
+- client_may_init - clients may set this property during creation (default: 1)
+- client_may_update - clients may set this property during update (default: 1)
+- is_optional - records do not have to have this property (default: 0)
+- is_virtual - this property is not stored in the database (default: 0)
+- validator - a different validator to use (default: per data type)
+- default_value - a default value for the property
+
+=method ix_property_info
+
+This returns a hashref the saved property information for the rclass.
+
+=cut
 
 my %IX_TYPE = (
   # idstr should get done this way in the future
@@ -191,6 +379,14 @@ my %DEFAULT_VALIDATOR = (
   boolean => Ix::Validators::boolean(),
   idstr   => Ix::Validators::idstr(),
 );
+
+=method ix_finalize
+
+This method is called by the schema's C<ix_finalize> method. It does a bunch of
+internal error checking, and throws fatal errors if you've messed up something
+in your rclass definition.
+
+=cut
 
 my %DID_FINALIZE;
 sub ix_finalize ($class) {
@@ -274,6 +470,70 @@ sub ix_finalize ($class) {
   }
 }
 
+=head1 HOOK METHODS
+
+L<Ix::DBIC::ResultSet> calls out to many methods defined in the rclass during
+normal JMAP method handlers. This allows your rclasses to customize their
+handling in lots of ways, without needing to subclass the ResultSet class
+itself. They come in several varieties:
+
+=begin :list
+
+= check methods (C<ix_FOO_check>)
+
+These are called I<before> the corresponding operation. If they return a value,
+that value must be an L<Ix::Error> object (probably, generated via
+C<< $ctx->error >>). This is especially useful for doing authorization checks.
+
+= error methods (C<ix_FOO_error>)
+
+The methods are passed any error, and expected to return a pair
+C<($row, $error)>. They are useful for modifying the result of an error, or for
+ignoring errors entirely.
+
+= transactional hook methods (C<ix_created>, C<ix_updated>, C<ix_destroyed>)
+
+These hooks are called inside the transaction where the create/update/destroy
+occurs, and passed the row object. They are most useful for performing side
+effects that might need to be rolled back if the transaction fails.
+
+= postprocess methods (C<ix_postprocess_FOO>)
+
+Thes hooks are called I<after> the create/update/destroy transaction occurs, and
+can be used to modify the results, or to perform side effects.
+
+=end :list
+
+=method ix_set_check($ctx, \%arg)
+
+A check method called before Foo/set operations.
+
+=method ix_create_check($ctx, \%record)
+
+A check method called before Foo/set#create operations.
+
+=method ix_update_check($ctx, $row, \%record)
+
+A check method called before Foo/set#update operations.
+
+=method ix_destroy_check($ctx, $row)
+
+A check method called before Foo/set#destroy operations.
+
+=method ix_changes_check($ctx, \%arg)
+
+A check method called before Foo/changes operations.
+
+=method ix_query_check($ctx, \%arg, \%search)
+
+A check method called before Foo/query operations.
+
+=method ix_query_changes_check($ctx, \%arg, \%search)
+
+A check method called before Foo/queryChanges operations.
+
+=cut
+
 sub ix_set_check { return; } # ($self, $ctx, \%arg)
 
 sub ix_get_check              { } # ($self, $ctx, \%arg)
@@ -282,8 +542,34 @@ sub ix_update_check           { } # ($self, $ctx, $row, \%rec)
 sub ix_destroy_check          { } # ($self, $ctx, $row)
 sub ix_changes_check          { } # ($self, $ctx, \%arg)
 
+=method ix_create_error($ctx, \%error)
+
+A hook method called when Foo/set#create encounters errors.
+
+=method ix_update_error($ctx, \%error)
+
+A hook method called when Foo/set#update encounters errors.
+
+=cut
+
 sub ix_create_error  { return; } # ($self, $ctx, \%error)
 sub ix_update_error  { return; } # ($self, $ctx, \%error)
+
+=method ix_created($ctx, $row)
+
+A hook method called inside the Foo/set#create transaction.
+
+=method ix_updated($ctx, $row, \%changes)
+
+A hook method called inside the Foo/set#update transaction. C<$changes> is a
+hashref with both C<old> and C<new> keys, so hooks can inspect the changes and
+take action only in certain cases.
+
+=method ix_destroyed($ctx, $row)
+
+A hook method called inside the Foo/set#destroy transaction.
+
+=cut
 
 sub ix_created   { } # ($self, $ctx, $row)
 sub ix_destroyed { } # ($self, $ctx, $row)
@@ -292,17 +578,56 @@ sub ix_destroyed { } # ($self, $ctx, $row)
 # present, so we don't define it in the base class. -- rjbs, 2017-01-06
 # sub ix_updated   { } # ($self, $ctx, $row, \%changes)
 
+=method ix_postprocess_create($self, $ctx, \@rows)
+
+A postprocess hook called after Foo/set#create operations.
+
+=method ix_postprocess_update($self, $ctx, \%updated)
+
+A postprocess hook called after Foo/set#create operations.
+
+=method ix_postprocess_destroy($self, $ctx, \@row_ids)
+
+A postprocess hook called after Foo/set#create operations.
+
+=cut
+
 sub ix_postprocess_create  { } # ($self, $ctx, \@rows)
 sub ix_postprocess_update  { } # ($self, $ctx, \%updated)
 sub ix_postprocess_destroy { } # ($self, $ctx, \@row_ids)
 
+# I am not going to document this publicly, but its signature is
+# ($self, $ctx, $get_arg, $results)
 sub _return_ix_get   { return $_[3]->@* }
 
+=method ix_update_state_string_field
+
+This defaults to C<modSeqChanged>, but if you like, you can override it to use
+some other field to define your rclass's state string.
+
+=cut
+
 sub ix_update_state_string_field { 'modSeqChanged' }
+
+=method ix_state_string($state)
+
+This is passed the context's C<state> attribute (an L<Ix::AccountState> object),
+and returns the state string for this object type. You can override it if you
+don't want to use the default integer state strings.
+
+=cut
 
 sub ix_state_string ($self, $state) {
   return $state->state_for( $self->ix_type_key ) . "";
 }
+
+=method ix_get_extra_search($ctx, $arg)
+
+Extra search arguments run during Foo/get operations.  If provided, this should
+return two hashrefs suitable for passing to DBIC's C<search> method: the first
+of search conditions, the second of search attributes.
+
+=cut
 
 sub ix_get_extra_search ($self, $ctx, $arg = {}) {
   return (
@@ -310,6 +635,14 @@ sub ix_get_extra_search ($self, $ctx, $arg = {}) {
     {},
   );
 }
+
+=method ix_update_extra_search($ctx, $arg)
+
+Extra search arguments run during Foo/set#update operations.  If provided, this
+should return two hashrefs suitable for passing to DBIC's C<search> method: the
+first of search conditions, the second of search attributes.
+
+=cut
 
 sub ix_update_extra_search ($self, $ctx, $arg) {
   my $since = $arg->{since};
@@ -329,22 +662,53 @@ sub ix_update_extra_search ($self, $ctx, $arg) {
   );
 }
 
+=method ix_update_extra_select
+
+Extra fields to select when running Foo/set#update searches. This is useful if
+your hook methods need to have access to additional data that wasn't necessarily
+provided by the client. If provided, should return an arrayref.
+
+=cut
+
 sub ix_update_extra_select {
   return [];
 }
+
+=method ix_highest_state($since, $rows)
+
+Returns the highest state for a given set of rows.
+
+=cut
 
 sub ix_highest_state ($self, $since, $rows) {
   my $state_string_field = $self->ix_update_state_string_field;
   return $rows->[-1]{$state_string_field};
 }
 
+=method ix_item_created_since($item, $since)
+
+Returns whether or not C<$item> was created after C<$since>. (You may need to
+override this method if you're not using the default state strings.)
+
+=cut
+
 sub ix_item_created_since ($self, $item, $since) {
   return $item->{modSeqCreated} > $since;
 }
 
+# XXX: when going through to document this code, I think it is now dead: it was
+# removed in aedf25370c, when we made the change that Foo/changes must return an
+# error if we can't compute a small enough response. -- michael, 2020-02-14
 sub ix_update_single_state_conds ($self, $example_row) {
   return { 'me.modSeqChanged' => $example_row->{modSeqChanged} }
 }
+
+=method ix_compare_state($since, $state)
+
+Used internally by L<Ix::DBIC::ResultSet> to determine how to act for
+Foo/changes.  Returns an L<Ix::StateComparison> object.
+
+=cut
 
 sub ix_compare_state ($self, $since, $state) {
   my $high_ms = $state->highest_modseq_for($self->ix_type_key);
@@ -362,6 +726,15 @@ sub ix_compare_state ($self, $since, $state) {
 
   return Ix::StateComparison->okay;
 }
+
+=method ix_create_base_state
+
+This method is called when you create a new account (i.e., an
+C<Ix::DBIC::Result> where C<ix_is_account_base> is true). After you create an
+account, you must call this method to insert the appropriate rows into the
+states table (so that it can be tracked by C<Ix::AccountState>.
+
+=cut
 
 sub ix_create_base_state ($self) {
   unless ($self->ix_is_account_base) {
