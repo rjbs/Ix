2306cbb7365912a58140353db365377f0f89940e
Michael McClimon <michael@mcclimon.org>
2020-02-14 10:37:08 -0500

    docs: Ix::App and Ix::App::JMAP

--- lib/Ix/App/JMAP.pm
+++ lib/Ix/App/JMAP.pm
@@ -1,5 +1,6 @@
 use 5.20.0;
 package Ix::App::JMAP;
+# ABSTRACT: an Ix::App for JMAP processors
 
 use Moose::Role;
 use experimental qw(signatures postderef);
@@ -10,6 +11,15 @@ use Try::Tiny;
 use namespace::autoclean;
 
 with 'Ix::App';
+
+=head1 OVERVIEW
+
+This is a Moose::Role which is just an L<Ix::App> whose C<_core_request>
+method is designed for JMAP. It only accepts JSON requests, passes them into
+C<handle_calls> on an C<Ix::Context> object, then encodes the JSON on the way
+out. Easy peasy.
+
+=cut
 
 sub _core_request ($self, $ctx, $req) {
   my $payload = try { $self->decode_json( $req->raw_body ); };
