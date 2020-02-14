4ccd0963b1b80f0d62b3775b61c8928d687bacc7
Michael McClimon <michael@mcclimon.org>
2020-02-14 10:00:36 -0500

    docs: Ix::Multicall

--- lib/Ix/Multicall/Done.pm
+++ lib/Ix/Multicall/Done.pm
@@ -1,6 +1,13 @@
 package Ix::Multicall::Done;
+# ABSTRACT: an L<Ix::Multicall> that requires no action
 
 use Moose;
+
+=method execute
+
+Simply returns the C<result_cid_pairs>.
+
+=cut
 
 sub execute {
   my ($self) = @_;
@@ -8,10 +15,21 @@ sub execute {
   return [ $self->result_cid_pairs ];
 }
 
+=attr call_ident
+
+=cut
+
 has call_ident => (
   is  => 'ro',
   isa => 'Str',
 );
+
+=attr result_cid_pairs
+
+An arrayref of C<[ $result, $client_id ]> pairs representing the JMAP method
+response.
+
+=cut
 
 has result_cid_pairs => (
   isa => 'ArrayRef',
